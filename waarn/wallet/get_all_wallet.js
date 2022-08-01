56f35d46eb282f8cf661d041f1a9e6a832409141
gaoey-log21ruby <peeravas.r@log21ruby.com>
2022-08-01 17:15:52 +0700

    feat: add load test API

---
+++ waarn/wallet/get_all_wallet.js
@@ -0,0 +1,23 @@
+import http from "k6/http";
+
+const config = JSON.parse(open(`../../env/waarn/${__ENV.APP_ENV}.json`));
+
+export const options = {
+  thresholds: {
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+    http_req_duration: ["p(95)<500"], // 95% of requests should be below 200ms
+  },
+};
+
+export default function () {
+  const url = `${config.base_url}/wallet`;
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${config.jwt}`,
+    },
+  };
+
+  http.get(url, params);
+}
+s;
